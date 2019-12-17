7c9c4d5bd20fd04b5bbc0259ec61cf6c1bcce299
appvissta <appvissta@gmail.com>
2019-12-17 20:59:30 +0530

    Code Commit

---
+++ Sol_Call_Action_Javascript/Sol_Call_Action_Javascript/Controllers/DemoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Call_Action_Javascript.Models;
+
+namespace Sol_Call_Action_Javascript.Controllers
+{
+    public class DemoController : Controller
+    {
+
+        public DemoController()
+        {
+            Users = new UserModel();
+        }
+
+
+        [BindProperty]
+        public UserModel Users { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OnGetJson()
+        {
+            Users.firstName = "Kishor";
+            Users.lastName = "Naik";
+
+            return base.Json(Users);
+        }
+
+        [HttpPost]
+        public IActionResult OnPostJson([FromBody]UserModel userModel)
+        {
+            return base.Json(userModel);
+        }
+    }
+}
---
+++ Sol_Call_Action_Javascript/Sol_Call_Action_Javascript/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Sol_Call_Action_Javascript.Models
+{
+    [DataContract]
+    public class UserModel
+    {
+        [DataMember(EmitDefaultValue =false)]
+        public String firstName { get; set; }
+
+        [DataMember(EmitDefaultValue =false)]
+        public String lastName { get; set; }
+    }
+}
